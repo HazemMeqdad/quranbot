fe2e3cf09958ec1c3b4b114cb150244455b99c98
HazemMeqdad <hazemmeqdad@gmail.com>
2021-06-13 19:14:32 +0300

    Update system send azkar

--- bot/events/loop.py
+++ bot/events/loop.py
@@ -6,34 +6,33 @@ import discord
 
 
 class Send(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, bot):
+        self.bot = bot
 
     @tasks.loop(minutes=30)
     async def send_30m(self):
         _all = db.All
         for i in _all.get_all_channels():
-            guild = self.client.get_guild(i[0])
+            guild = self.bot.get_guild(i[0])
             if guild is None:
                 continue
-            x = db.Guild(guild)
-            channel = self.client.get_channel(x.info[3])
+            channel = self.bot.get_channel(i[3])
             if channel is None:
                 continue
-            if x.info[4] == 1800:
-                if bool(x.info[5]):
+            if i[4] == 1800:
+                if bool(i[5]):
                     try:
                         message = await channel.fetch_message(channel.last_message_id)
-                        if message.author == self.client.user:
+                        if message.author == self.bot.user:
                             continue
                     except:
                         continue
                 try:
-                    if bool(x.info[6]):
+                    if bool(i[6]):
                         await channel.send(embed=discord.Embed(
                             description=random.choice(config.all),
                             color=discord.Color.gold()
-                        ).set_footer(text=self.client.user.name, icon_url=guild.icon_url))
+                        ).set_footer(text=self.bot.user.name, icon_url=guild.icon_url))
                         continue
                     await channel.send(random.choice(config.all))
                 except:
@@ -43,27 +42,26 @@ class Send(commands.Cog):
     async def send_1h(self):
         _all = db.All
         for i in _all.get_all_channels():
-            guild = self.client.get_guild(i[0])
+            guild = self.bot.get_guild(i[0])
             if guild is None:
                 continue
-            x = db.Guild(guild)
-            channel = self.client.get_channel(x.info[3])
+            channel = self.bot.get_channel(i[3])
             if channel is None:
                 continue
-            if x.info[4] == 3600:
-                if bool(x.info[5]):
+            if i[4] == 3600:
+                if bool(i[5]):
                     try:
                         message = await channel.fetch_message(channel.last_message_id)
-                        if message.author == self.client.user:
+                        if message.author == self.bot.user:
                             continue
                     except:
                         continue
                 try:
-                    if bool(x.info[6]):
+                    if bool(i[6]):
                         await channel.send(embed=discord.Embed(
                             description=random.choice(config.all),
                             color=discord.Color.gold()
-                        ).set_footer(text=self.client.user.name, icon_url=guild.icon_url))
+                        ).set_footer(text=self.bot.user.name, icon_url=guild.icon_url))
                         continue
                     await channel.send(random.choice(config.all))
                 except:
@@ -73,27 +71,26 @@ class Send(commands.Cog):
     async def send_2h(self):
         _all = db.All
         for i in _all.get_all_channels():
-            guild = self.client.get_guild(i[0])
+            guild = self.bot.get_guild(i[0])
             if guild is None:
                 continue
-            x = db.Guild(guild)
-            channel = self.client.get_channel(x.info[3])
+            channel = self.bot.get_channel(i[3])
             if channel is None:
                 continue
-            if x.info[4] == 7200:
-                if bool(x.info[5]):
+            if i[4] == 7200:
+                if bool(i[5]):
                     try:
                         message = await channel.fetch_message(channel.last_message_id)
-                        if message.author == self.client.user:
+                        if message.author == self.bot.user:
                             continue
                     except:
                         continue
                 try:
-                    if bool(x.info[6]):
+                    if bool(i[6]):
                         await channel.send(embed=discord.Embed(
                             description=random.choice(config.all),
                             color=discord.Color.gold()
-                        ).set_footer(text=self.client.user.name, icon_url=guild.icon_url))
+                        ).set_footer(text=self.bot.user.name, icon_url=guild.icon_url))
                         continue
                     await channel.send(random.choice(config.all))
                 except:
@@ -103,27 +100,26 @@ class Send(commands.Cog):
     async def send_6h(self):
         _all = db.All
         for i in _all.get_all_channels():
-            guild = self.client.get_guild(i[0])
+            guild = self.bot.get_guild(i[0])
             if guild is None:
                 continue
-            x = db.Guild(guild)
-            channel = self.client.get_channel(x.info[3])
+            channel = self.bot.get_channel(i[3])
             if channel is None:
                 continue
-            if x.info[4] == 21600:
-                if bool(x.info[5]):
+            if i[4] == 21600:
+                if bool(i[5]):
                     try:
                         message = await channel.fetch_message(channel.last_message_id)
-                        if message.author == self.client.user:
+                        if message.author == self.bot.user:
                             continue
                     except:
                         continue
                 try:
-                    if bool(x.info[6]):
+                    if bool(i[6]):
                         await channel.send(embed=discord.Embed(
                             description=random.choice(config.all),
                             color=discord.Color.gold()
-                        ).set_footer(text=self.client.user.name, icon_url=guild.icon_url))
+                        ).set_footer(text=self.bot.user.name, icon_url=guild.icon_url))
                         continue
                     await channel.send(random.choice(config.all))
                 except:
@@ -133,27 +129,26 @@ class Send(commands.Cog):
     async def send_12h(self):
         _all = db.All
         for i in _all.get_all_channels():
-            guild = self.client.get_guild(i[0])
+            guild = self.bot.get_guild(i[0])
             if guild is None:
                 continue
-            x = db.Guild(guild)
-            channel = self.client.get_channel(x.info[3])
+            channel = self.bot.get_channel(i[3])
             if channel is None:
                 continue
-            if x.info[4] == 43200:
-                if bool(x.info[5]):
+            if i[4] == 43200:
+                if bool(i[5]):
                     try:
                         message = await channel.fetch_message(channel.last_message_id)
-                        if message.author == self.client.user:
+                        if message.author == self.bot.user:
                             continue
                     except:
                         continue
                 try:
-                    if bool(x.info[6]):
+                    if bool(i[6]):
                         await channel.send(embed=discord.Embed(
                             description=random.choice(config.all),
                             color=discord.Color.gold()
-                        ).set_footer(text=self.client.user.name, icon_url=guild.icon_url))
+                        ).set_footer(text=self.bot.user.name, icon_url=guild.icon_url))
                         continue
                     await channel.send(random.choice(config.all))
                 except:
@@ -163,27 +158,26 @@ class Send(commands.Cog):
     async def send_24h(self):
         _all = db.All
         for i in _all.get_all_channels():
-            guild = self.client.get_guild(i[0])
+            guild = self.bot.get_guild(i[0])
             if guild is None:
                 continue
-            x = db.Guild(guild)
-            channel = self.client.get_channel(x.info[3])
+            channel = self.bot.get_channel(i[3])
             if channel is None:
                 continue
-            if x.info[4] == 86400:
-                if bool(x.info[5]):
+            if i[4] == 86400:
+                if bool(i[5]):
                     try:
                         message = await channel.fetch_message(channel.last_message_id)
-                        if message.author == self.client.user:
+                        if message.author == self.bot.user:
                             continue
                     except:
                         continue
                 try:
-                    if bool(x.info[6]):
+                    if bool(i[6]):
                         await channel.send(embed=discord.Embed(
                             description=random.choice(config.all),
                             color=discord.Color.gold()
-                        ).set_footer(text=self.client.user.name, icon_url=guild.icon_url))
+                        ).set_footer(text=self.bot.user.name, icon_url=guild.icon_url))
                         continue
                     await channel.send(random.choice(config.all))
                 except:
@@ -200,5 +194,5 @@ class Send(commands.Cog):
         self.send_24h.start()
 
 
-def setup(client):
-    client.add_cog(Send(client))
+def setup(bot):
+    bot.add_cog(Send(bot))
