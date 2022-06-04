70c4cb92dbe0d6185567348ff2abd7b805d7a7b4
HazemMeqdad <hazemmeqdad@gmail.com>
2022-06-04 19:32:45 +0300

    Make a pram without context

--- bot/utils/voice.py
+++ bot/utils/voice.py
@@ -1,35 +1,36 @@
-import logging
-from hikari.api.voice import VoiceConnection
 from hikari.snowflakes import Snowflake
-from lightbulb.context import Context
+import lightbulb
 import hikari
 import typing as t
+import lavaplayer
 
 
-async def join_voice_channel(ctx: Context) -> t.Union[Snowflake, hikari.Embed]:
-    states = ctx.bot.cache.get_voice_states_view_for_guild(ctx.get_guild())
+async def join_voice_channel(bot: lightbulb.BotApp, guild: hikari.SnowflakeishOr[hikari.Guild], auther: t.Union[hikari.Member, hikari.User]) -> t.Union[Snowflake, hikari.Embed]:
+    states = bot.cache.get_voice_states_view_for_guild(guild)
     voice_state = list(
-        filter(lambda i: i.user_id == ctx.author.id, states.iterator())
+        filter(lambda i: i.user_id == auther.id, states.iterator())
     )
-    embed = hikari.Embed(color=0xffd430)
     if not voice_state:
-        embed.description = "يجب عليك دخول غرفه صوتيه"
-        return embed
+        return None
     
     channel_id = voice_state[0].channel_id
-    await ctx.bot.update_voice_state(ctx.get_guild(), channel_id, self_deaf=True)
-    await ctx.bot.lavalink.wait_for_connection(ctx.guild_id)
+    await bot.update_voice_state(guild, channel_id, self_deaf=True)
+    await bot.lavalink.wait_for_connection(guild)
     return channel_id 
 
-async def leave_and_stop(ctx: Context):
-    states = ctx.bot.cache.get_voice_states_view_for_guild(ctx.get_guild())
+async def leave_and_stop(bot: lightbulb.BotApp, guild: hikari.SnowflakeishOr[hikari.Guild]):
+    states = bot.cache.get_voice_states_view_for_guild(guild)
     voice_state = list(
-        filter(lambda i: i.user_id == ctx.bot.get_me().id, states.iterator())
+        filter(lambda i: i.user_id == bot.get_me().id, states.iterator())
     )
 
     if not voice_state:
         return
-    await ctx.bot.update_voice_state(ctx.guild_id, None)
-    await ctx.bot.lavalink.wait_for_remove_connection(ctx.guild_id)
+    await bot.update_voice_state(guild, None)
+    await bot.lavalink.wait_for_remove_connection(guild)
     return voice_state[0].channel_id
 
+async def play_lavalink_source(lavalink: lavaplayer.LavalinkClient, guild: hikari.SnowflakeishOr[hikari.Guild], source: str, user: hikari.SnowflakeishOr[hikari.User]):
+    tracks = await lavalink.get_tracks(source)
+    guild_id = guild.id if isinstance(guild, hikari.Guild) else guild
+    await lavalink.play(guild_id, tracks[0], user.id)
