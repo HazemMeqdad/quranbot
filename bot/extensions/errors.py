809f605fc9f655027923746df0947fc79d30a2e3
HazemMeqdad <hazemmeqdad@gmail.com>
2022-06-06 18:04:33 +0300

    Fix command error handler responed

--- bot/extensions/errors.py
+++ bot/extensions/errors.py
@@ -18,23 +18,11 @@ async def on_slash_command_error_event(event: lightbulb.SlashCommandErrorEvent):
         await event.context.respond(embed=embed)
         return
     elif event.context.bot.lavalink.is_connect == False:
+        embed.description = "يرجى إعادة كتابة الأمر من جديد"
+        await event.context.respond(embed=embed)
         await event.bot.create_lavalink_connection()
-        await leave_and_stop(event.context)
-        await event.context.respond("يرجا إعادة كتابة الأمر مره أخره")
-        return
-    if pathlib.Path("errors.json").exists():
-        with open("errors.json", "r") as f:
-            errors = json.load(f)
-        errors.append({
-            "message": event.exception.__str__(),
-            "traceback": event.exception.__traceback__.__str__(),
-            "timestamp": str(datetime.datetime.now()),
-            "channel": event.context.channel_id.__str__(),
-            "guild": event.context.guild_id.__str__(),
-        })
-        with open("errors.json", "w") as f:
-            json.dump(errors, f, indent=4)
-            
+        await leave_and_stop(error_plugin.bot, event.context.guild_id)
+        return   
     embed.description = event.exception.__str__()
     await event.context.respond(embed=embed)
 
