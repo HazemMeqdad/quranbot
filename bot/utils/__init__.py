39922ad4aaebc26ffa896d3ba4c45762246026a5
HazemMeqdad <hazemmeqdad@gmail.com>
2022-08-25 10:53:48 +0300

    /\

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,8 +1,5 @@
 from .emojis import Emojis
 from .prayer import Prayer
 from .voice import join_voice_channel, leave_and_stop
-import hikari
 
-__all__ = [Emojis, Prayer, join_voice_channel, leave_and_stop]
-
-
+__all__ = ["Emojis", "Prayer", "join_voice_channel", "leave_and_stop"]
