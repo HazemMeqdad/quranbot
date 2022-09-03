c9b7b31134cc3f1815c4614b373668828eaddb43
HazemMeqdad <hazemmeqdad@gmail.com>
2022-09-03 06:41:16 +0300

    Fix a eval command to use a async code

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,3 +1,4 @@
+import inspect
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -8,20 +9,7 @@ class Owner(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="restart", description="Reloads a cog.")
-    @commands.is_owner()
-    async def all_reload(self, interaction: discord.Interaction) -> None:
-        """Reloads a cog."""
-        for cog in self.bot.cogs:
-            try:
-                await self.bot.reload_extension(cog)
-            except Exception as e:
-                await interaction.response.send_message(f"Failed to reload cog {cog}: {e}")
-            else:
-                await interaction.response.send_message(f"Reloaded cog {cog}")
-
     @app_commands.command(name="load", description="Loads a cog.")
-    @commands.is_owner()
     async def load(self, interaction: discord.Interaction, cog: str) -> None:
         """Loads a cog."""
         try:
@@ -32,7 +20,6 @@ class Owner(commands.Cog):
             await interaction.response.send_message(f"Loaded cog {cog}")
         
     @app_commands.command(name="unload", description="Unloads a cog.")
-    @commands.is_owner()
     async def unload(self, interaction: discord.Interaction, cog: str) -> None:
         """Unloads a cog."""
         try:
@@ -43,7 +30,6 @@ class Owner(commands.Cog):
             await interaction.response.send_message(f"Unloaded cog {cog}")
         
     @app_commands.command(name="reload", description="Reloads a cog.")
-    @commands.is_owner()
     async def reload(self, interaction: discord.Interaction, cog: str) -> None:
         """Reloads a cog."""
         try:
@@ -54,15 +40,16 @@ class Owner(commands.Cog):
             await interaction.response.send_message(f"Reloaded cog {cog}")
         
     @app_commands.command(name="eval", description="Evaluates a code.")
-    @commands.is_owner()
     async def eval(self, interaction: discord.Interaction, *, code: str) -> None:
         """Evaluates a code."""
         try:
             result = eval(code)
+            if inspect.isawaitable(result):
+                result = await result
         except Exception as e:
             await interaction.response.send_message(f"Failed to evaluate code: {e}")
         else:
-            await interaction.response.send_message(f"Evaluated code: {result}")
+            await interaction.response.send_message(f"{result}")
 
 
 async def setup(bot: commands.Bot) -> None:
